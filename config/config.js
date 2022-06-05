f2c60c430b7e5b711b177f2e78ed26f121c1ff78
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2022-06-05 11:06:05 +0700

    deploy

--- config/config.js
+++ config/config.js
@@ -21,7 +21,6 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     dialectOptions: {
-      supportBigNumbers: true,
       ssl: {
         require: true,
         rejectUnauthorized: false,
--- models/users.js
+++ models/users.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
       name: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
-      phone: DataTypes.INTEGER,
+      phone: DataTypes.STRING,
       alamat: DataTypes.STRING,
       photo_profile: DataTypes.STRING,
       hasStore: DataTypes.BOOLEAN,
