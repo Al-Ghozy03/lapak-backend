f4bf7785cdf9fd00c5fa2bec030d9bfb13bacde7
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2022-06-12 19:55:06 +0700

    ok

--- models/stores.js
+++ models/stores.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
       
       stores.belongsTo(models.barangs, {
         as: "toko",
-        foreignKey: "id",
+        foreignKey: "store_id",
       });
     }
   }
