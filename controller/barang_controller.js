890d5193e9656fb895fde0e9ddde34c88bf1faac
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2022-06-12 14:22:28 +0700

    ok

--- controller/barang_controller.js
+++ controller/barang_controller.js
@@ -77,7 +77,7 @@ async function testing(req, res) {
   try {
     let { orderBy, item } = req.query;
     const data = await sequelize.query(
-      `select * from barangs`,
+      `select * from barangs where nama_barang like "%lap%"`,
       {
         type: QueryTypes.SELECT,
         raw: true,
