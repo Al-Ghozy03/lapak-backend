74e81769e215cd546025da50531f4bf8cb1643d0
Muhammad Faiz Al Ghozi <muhammadfaizalghozi1@gmail.com>
2022-06-12 19:46:36 +0700

    ok

--- models/barangs.js
+++ models/barangs.js
@@ -8,8 +8,6 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: "store_id",
         through: "",
       });
-      barangs.hasMany(models.stores,{as:"toko",foreignKey:"id"})
-      
       barangs.belongsToMany(models.orders, {
         as: "order_barang",
         foreignKey: "barang_id",
--- models/stores.js
+++ models/stores.js
@@ -9,12 +9,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       stores.hasMany(models.barangs, { as: "barang", foreignKey: "store_id" });
-
-      stores.belongsToMany(models.barangs, {
-        as: "toko",
-        foreignKey: "id",
-        through: "",
-      });
     }
   }
   stores.init(
